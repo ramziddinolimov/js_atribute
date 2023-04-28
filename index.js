bb04877efe882ea4a2d48d3500bb6a481918fe27
ramziddinolimov <ramizmafiya@gmail.com>
2023-04-28 08:46:34 +0500

    has

---
+++ index.js
@@ -0,0 +1,8 @@
+const img = document.querySelector("#img")
+const btn = document.querySelector("#btn")
+
+btn.addEventListener("click", event => {
+    has = img.hasAttribute("width")
+    console.log(has);
+    // Bor bolsa true chiqaradi bolmasa false
+})
