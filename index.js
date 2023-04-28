4fdc9b2f8fe2c6ccca2a5d898883d2c5d5d69227
ramziddinolimov <ramizmafiya@gmail.com>
2023-04-28 08:55:18 +0500

    get

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const img = document.querySelector("#img")
-const btn = document.querySelector("#btn")
+const btn1 = document.querySelector("#btn1")
+const btn2 = document.querySelector("#btn2")
+const btn3 = document.querySelector("#btn3")
 
-btn.addEventListener("click", event => {
+btn1.addEventListener("click", event => {
     has = img.hasAttribute("width")
     console.log(has);
     // Bor bolsa true chiqaradi bolmasa false
+})
+
+btn2.addEventListener("click", event => {
+    has = img.getAttribute("width")
+    console.log(has);
+    // Bor bolsa atributni valuesini ob beradi 
+})
+
+btn2.addEventListener("click", event => {
+    has = img.getAttribute("width")
+    console.log(has);
+    // Bor bolsa atributni valuesini ob beradi 
 })
